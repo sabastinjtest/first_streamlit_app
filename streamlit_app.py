05dadf2435e2a4f10b7fbccd2f441d58a92404d2
sabastinjtest <34360601+sabastinjtest@users.noreply.github.com>
2023-06-02 11:53:58 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,9 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to show information')
   else:
-    back_from_function = get_fruitvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
+    #back_from_function = get_fruitvice_data(fruit_choice)
+    #streamlit.dataframe(back_from_function)
+    streamlit.dataframe(get_fruitvice_data(fruit_choice))
 
 
 
